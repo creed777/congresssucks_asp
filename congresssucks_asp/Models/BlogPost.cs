cdda6db6a2e9909007c68d350e9be630e8688f55
Chris Reed <chris.reed777@gmail.com>
2019-10-05 14:20:53 -0500

    JS works with tinyMCE on Create

--- congresssucks_asp/Models/BlogPost.cs
+++ congresssucks_asp/Models/BlogPost.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace congresssucks_asp.Models
 {
@@ -12,8 +13,10 @@ namespace congresssucks_asp.Models
         [Required]
         public string Title { get; set; }
         [Required]
+        [AllowHtml]
         public string ShortPost { get; set; }
         [Required]
+        [AllowHtml]
         public string Post { get; set; }
         [Required]
         public string Tags { get; set; }
